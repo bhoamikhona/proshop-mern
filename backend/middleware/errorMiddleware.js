84c0247fa29a546c586ebda0785985a5bd3eb6e9
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-23 01:32:30 +0530

    custom error handling middleware

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,22 @@
+const notFound = function (req, res, next) {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = function (err, req, res, next) {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,9 @@ connectDB();
 app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`App started listening on port ${port}`));
