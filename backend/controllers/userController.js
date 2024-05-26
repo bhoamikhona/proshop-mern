1ca17a1f73e2c57bbc00cd18001fcd314ac386f3
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-26 12:15:04 +0530

    logout user and clear cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -52,7 +52,12 @@ const registerUser = asyncHandler(async function (req, res) {
  * @access  Private
  */
 const logoutUser = asyncHandler(async function (req, res) {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 /**
