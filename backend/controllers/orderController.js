579fd7a639bdcea0a39d1c4c5f04a58b163f1c3a
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-29 15:00:01 +0530

    orders list UI

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -111,7 +111,8 @@ const updateOrderToDelivered = asyncHandler(async function (req, res) {
  * @access  Private/Admin
  */
 const getOrders = asyncHandler(async function (req, res) {
-  res.send("get all orders");
+  const orders = await Order.find({}).populate("user", "id name");
+  res.status(200).json(orders);
 });
 
 export {
