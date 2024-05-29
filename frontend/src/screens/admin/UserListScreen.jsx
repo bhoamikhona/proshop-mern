41906eb3d9147f1841470a9153745063c6820a7f
Bhoami K Khona <bhoamikhona@gmail.com>
2024-05-30 00:00:19 +0530

    delete users - admin functionality - frontend

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -4,17 +4,33 @@ import { Table, Button } from "react-bootstrap";
 import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
 import Message from "../../components/Message.jsx";
 import Loader from "../../components/Loader.jsx";
-import { useGetUsersQuery } from "../../slices/usersApiSlice.js";
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+} from "../../slices/usersApiSlice.js";
 import { toast } from "react-toastify";
 
 function UserListScreen() {
   const { data: users, refetch, isLoading, error } = useGetUsersQuery();
 
-  const deleteHandler = async function (id) {};
+  const [deleteUser, { isLoading: loadingDelete }] = useDeleteUserMutation();
+
+  const deleteHandler = async function (id) {
+    if (window.confirm("Are you sure?")) {
+      try {
+        await deleteUser(id);
+        refetch();
+        toast.success("User Deleted");
+      } catch (error) {
+        toast.error(error?.data?.message || error.error);
+      }
+    }
+  };
 
   return (
     <>
       <h1>Users</h1>
+      {loadingDelete && <Loader />}
       {isLoading ? (
         <Loader />
       ) : error ? (
@@ -48,19 +64,19 @@ function UserListScreen() {
                   )}
                 </td>
                 <td>
-                  {!user.isAdmin && (
-                    <div className="d-flex align-items-center justify-content-center">
-                      <Link
-                        to={`/admin/user/${user.id}/edit`}
-                        style={{ marginRight: "10px" }}
+                  <div className="d-flex align-items-center justify-content-start mx-2">
+                    <Link
+                      to={`/admin/user/${user.id}/edit`}
+                      style={{ marginRight: "10px" }}
+                    >
+                      <Button
+                        variant="primary"
+                        className="btn-sm d-flex align-items-center"
                       >
-                        <Button
-                          variant="primary"
-                          className="btn-sm d-flex align-items-center"
-                        >
-                          <FaEdit color="white" />
-                        </Button>
-                      </Link>
+                        <FaEdit color="white" />
+                      </Button>
+                    </Link>
+                    {!user.isAdmin && (
                       <Button
                         variant="danger"
                         className="btn-sm d-flex align-items-center"
@@ -68,8 +84,8 @@ function UserListScreen() {
                       >
                         <FaTrash style={{ color: "white" }} />
                       </Button>
-                    </div>
-                  )}
+                    )}
+                  </div>
                 </td>
               </tr>
             ))}
